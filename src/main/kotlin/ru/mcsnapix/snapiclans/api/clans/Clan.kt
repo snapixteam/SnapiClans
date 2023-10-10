5ffa02a5def65344df19f734cbab8f233ac4c716
Flaimer2 <63396386+Flaimer2@users.noreply.github.com>
2023-10-10 17:54:34 +0300

    fix

--- src/main/kotlin/ru/mcsnapix/snapiclans/api/clans/Clan.kt
+++ src/main/kotlin/ru/mcsnapix/snapiclans/api/clans/Clan.kt
@@ -4,9 +4,9 @@ import co.aikar.idb.DbRow
 import ru.mcsnapix.snapiclans.database.Database
 
 data class Clan(private val dbRow: DbRow) {
-    val id = dbRow.getInt("id")!!
-    val name = dbRow.getString("name")!!
-    val displayName = dbRow.getString("display_name")!!
-    val owner = dbRow.getString("owner")!!
+    val id: Int = dbRow.getInt("id")
+    val name: String = dbRow.getString("name")
+    val displayName: String = dbRow.getString("display_name")
+    val owner: String = dbRow.getString("owner")
     val members: List<ClanUser> = Database.users(id)
 }
--- src/main/kotlin/ru/mcsnapix/snapiclans/api/clans/ClanUser.kt
+++ src/main/kotlin/ru/mcsnapix/snapiclans/api/clans/ClanUser.kt
@@ -3,13 +3,14 @@ package ru.mcsnapix.snapiclans.api.clans
 import co.aikar.idb.DbRow
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import ru.mcsnapix.snapiclans.api.clans.role.ClanRole
 import ru.mcsnapix.snapiclans.api.exceptions.RoleDontFoundInConfigException
 import ru.mcsnapix.snapiclans.registry.RolesRegistry
 
 data class ClanUser(private val dbRow: DbRow) {
-    val clanId = dbRow.getInt("clan_id")!!
-    val username = dbRow.getString("username")!!
+    val clanId: Int = dbRow.getInt("clan_id")
+    val username: String = dbRow.getString("username")
     val player: Player?
         get() = Bukkit.getPlayer(username)
-    val role = RolesRegistry.clanRoles[dbRow.getString("role")] ?: throw RoleDontFoundInConfigException()
+    val role: ClanRole = RolesRegistry.clanRoles[dbRow.getString("role")] ?: throw RoleDontFoundInConfigException()
 }
